6820da848a242846aa3f1f00cb07a7f4e54b4e79
sixtine caspar <sixcaspar@gmail.com>
2024-09-02 11:26:53 +0200

    small seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,17 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
+psg = Team.create(name: "PSG")
+manchester = Team.create(name: "Manchester")
+real_madrid = Team.create(name: "Real Madrid")
+
+Player.create(name: "Kylian Mbappé", team_id: psg.id)
+Player.create(name: "Lionel Messi", team_id: psg.id)
+Player.create(name: "Neymar", team_id: psg.id)
+Player.create(name: "Kylian Mbappé", team_id: manchester.id)
+Player.create(name: "Lionel Messi", team_id: manchester.id)
+Player.create(name: "Zizou", team_id: manchester.id)
+Player.create(name: "Sixtine", team_id: real_madrid.id)
+Player.create(name: "Romuald", team_id: real_madrid.id)
+Player.create(name: "Stephou", team_id: real_madrid.id)
