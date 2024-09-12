deb00f354c7229f1a1bad1c4ce42bf4aa4d4daa5
sixtine caspar <sixcaspar@gmail.com>
2024-09-12 18:42:53 +0200

    add methods for team controller

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -1,12 +1,53 @@
+# frozen_string_literal: true
+
 class Api::TeamsController < ApplicationController
   def index
     teams = Team.includes(:players).map do |team|
       {
         id: team.id,
-        name: team.name,
-        players: team.players.pluck(:name)
+        name: team.name
       }
     end
     render json: teams
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    team = Team.new(team_params)
+    if team.save
+      render json: team
+    else
+      render json: { errors: team.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    team = Team.find(params[:id])
+
+    if team.update(name: params[:new_name])
+      render json: team
+    else
+      render json: { errors: team.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    team = Team.find(params[:id])
+    team.destroy
+    render json: { message: 'Team deleted' }
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(
+      :name,
+      :id,
+      :created_at,
+      :updated_at
+    )
   end
 end
